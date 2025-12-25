3da4b5edd6fe39bdb9d9fed83cfc653db01f76fa
Sai kamal <girisaikamal16@gmail.com>
2025-12-25 12:51:17 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function detectDisease() {
+  document.getElementById("diseaseResult").innerText =
+    "Disease: Leaf Blight (Confidence: 94%)";
+}
+
+function recommendCrop() {
+  document.getElementById("cropResult").innerText =
+    "Recommended Crop: Groundnut";
+}
+
+function askBot() {
+  document.getElementById("botAnswer").innerText =
+    "Use neem-based pesticide once a week.";
+}
